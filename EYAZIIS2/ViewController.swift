cbba68e6ad807d7ae9e2cdffdaedd141715d0315
RakhmatovBeymamat <beymamatrakhmatov@gmail.com>
2023-12-08 04:25:28 +0300

    Initial Commit

---
+++ EYAZIIS2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EYAZIIS2
+//
+//  Created by Rakhmatov Beymamat on 8.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EYAZIIS2UITests/EYAZIIS2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EYAZIIS2UITestsLaunchTests.swift
+//  EYAZIIS2UITests
+//
+//  Created by Rakhmatov Beymamat on 8.12.23.
+//
+
+import XCTest
+
+final class EYAZIIS2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
